9646d4d24d75a1344ecfd38f21bee4bc4f114842
Soumik Bhattacharyya <42136372+Soumik77777@users.noreply.github.com>
2020-10-04 23:39:07 +0530

    Add files via upload

---
+++ 2_laguerre.py
@@ -0,0 +1,24 @@
+from library_week5 import *
+import math
+
+a= [-18, 27, -7, -3, 1]
+
+guess = float(input("Enter your initial guess:"))
+
+print("Solutions of the polynomial equation are:")
+a=laguerre(poly_function,first_derivative,s_derivative,a,guess)
+
+a=laguerre(poly_function,first_derivative,s_derivative,a,guess)
+
+a=laguerre(poly_function,first_derivative,s_derivative,a,guess)
+
+a=laguerre(poly_function,first_derivative,s_derivative,a,guess)
+
+'''
+Enter your initial guess:2.3
+Solutions of the polynomial equation are:
+2.0000005707667103
+3.000031671048423
+0.9999529659652182
+-2.9999852077803517
+'''
